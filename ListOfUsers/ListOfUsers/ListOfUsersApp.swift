2a33422cc2633fa3186f80fdb0b245c86d563e4e
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-06-01 10:35:39 +0200

    initial commit

---
+++ ListOfUsers/ListOfUsers/ListOfUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfUsersApp.swift
+//  ListOfUsers
+//
+//  Created by Ahmed Fayeq on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListOfUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
