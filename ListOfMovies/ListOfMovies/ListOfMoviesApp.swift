bd1a9184745a1424c3a24863aae907edd8c77415
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2023-08-14 13:44:08 +0300

    initial commit

---
+++ ListOfMovies/ListOfMovies/ListOfMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfMoviesApp.swift
+//  ListOfMovies
+//
+//  Created by Ahmed Fayek on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListOfMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
