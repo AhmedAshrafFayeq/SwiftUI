bbf40b61d493009cbdabbea3c77cdcdcd908a85e
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2024-03-10 14:18:04 +0300

    create memoriza proj

---
+++ Memoriza/Memoriza/MemorizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizaApp.swift
+//  Memoriza
+//
+//  Created by Ahmed Fayek on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
